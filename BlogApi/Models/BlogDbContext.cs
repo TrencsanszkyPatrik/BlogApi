72c00adbe9a935b85b453a32e6b0cfcc09a9b053
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2025-11-10 08:59:07 +0100

    DbContext is created.

---
+++ BlogApi/Models/BlogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext()
+        {
+
+        }
+
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Blogger> bloggers { get; set; }
+    }
+}
